2c92f12b6a465c8201e852cd68ef593d9f322133
Riya Gupta <riyagupta3825@gmail.com>
2025-01-07 23:14:30 +0530

    initialized agent function

--- app.py
+++ app.py
@@ -15,3 +15,22 @@ API_KEY = os.getenv("GOOGLE_API_KEY")
 
 if API_KEY:
     genai.configure(api_key="API_KEY")
+
+st.set_page_config(
+    page_title="Multi Modal AI Agent - Video Summarizer",
+    layout="wide"
+)
+
+st.title("Phidata Video Summarizer")
+st.header("Powered by Gemini 2.0 Flash Exp")
+
+def initialize_agent():
+    return Agent(
+        name="AI Video Summarizer",
+        model=Gemini(id="gemini-2.0-flash-exp"),
+        tools=[DuckDuckGo()],
+        markdown=True
+    )
+
+multimodal_agent = initialize_agent()
+
